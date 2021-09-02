6485bb736a6d1bc12ee9450cf17a95c2a758609d
Rod Boev <rod.boev@gmail.com>
2021-09-02 13:36:41 -0400

    Search by location string instead of zip, fix zips data

--- highrisk.js
+++ highrisk.js
@@ -2,27 +2,29 @@ const mongoose = require('mongoose');
 
 const api = require('./api');
 const schema = require('./schema');
-const locations = require('./locations');
 
 const businessSchema = new mongoose.Schema(schema);
 const Business = mongoose.model('Business', businessSchema);
 
+const categories = ['debtrelief', 'travelagents', 'web_design', 'lifecoach', 'matchmakers', 'headshops', 'vapeshops', 'adultentertainment', 'stripclubs', 'pawn', 'paydayloans']
+
 async function main() {
-  await mongoose.connect('mongodb://localhost:27017/yelp');
-  let i, saved, dropped, total;
+  await mongoose.connect('mongodb://localhost:27017/highrisk');
+  let i = 0, savedNow = 0, savedTotal = 0, total = 0, droppedTotal = 0;
 
   const cacheLength = await api.cache.length();
   console.log('Cache length: ' + cacheLength);
 
-  for (location of locations) {
+  for (category of categories) {
     i = 0;
-    saved = 0;
-    dropped = 0;
+    savedNow = 0;
+    droppedNow = 0;
     total = 0;
 
     do {
       const params = {
-        location,
+        categories: category,
+        location: "New York, NY",
         limit: 50,
         offset: (i * 50)
       };
@@ -37,27 +39,32 @@ async function main() {
         for (const result of results) {
           const filter = { id: result.id };
           const update = result;
+          await Business.countDocuments(filter); // 0
 
           let doc = await Business.findOneAndUpdate(filter, update, {
             new: true,
             upsert: true // Make this update into an upsert
           });
-          saved++;
+          savedNow++;
+          savedTotal++;
         }
       }
       catch (err) {
         console.log(err.message);
       }
 
-      console.log(`Saved ${saved} out of ${total} records to DB.`);
+      console.log(`Saved ${savedNow} out of ${total} records to DB.`);
       i++;
     }
     while (i < Math.ceil(total / 50) && (i * 50 < 1000));
 
-    dropped = total > 1000 ? total - 1000 : 0;
-    dropped && console.log(`Dropped ${dropped} records due to limit.`)
+    droppedNow = total > 1000 ? total - 1000 : 0;
+    droppedTotal += droppedNow;
+    console.log(`Dropped ${droppedNow} records due to limit.`)
   }
 
+  console.log(`Total saved records: ${savedTotal}`);
+  console.log(`Total dropped records: ${droppedTotal}`);
 }
 
 main().catch(err => console.log(err));
