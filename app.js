3a1d4bf381cb09e2788c165c3f56a4c341656b32
Rod Boev <rod.boev@gmail.com>
2021-09-02 01:31:57 -0400

    Add schema

--- app.js
+++ app.js
@@ -1,20 +1,38 @@
 const axios = require('axios');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+const schema = require('./schema.js');
 
 dotenv.config();
-const apiKey = process.env.API_KEY;
 
+// Yelp
+const apiKey = process.env.API_KEY;
 const instance = axios.create({
   baseURL: 'https://api.yelp.com/v3/businesses/search',
   headers: { 'Authorization': 'Bearer ' + apiKey }
 });
 
-(async () => {
-  const response = await instance({
-    params: {
-      location: '11214'
-    }
-  })
+async function searchYelp(params) {
+  const response = await instance({ params });
+  // console.log(JSON.stringify(response.data, null, 4))
+  return response.data.businesses;
+}
 
-  console.log(JSON.stringify(response.data, null, 4))
-})();
+// MongoDB
+const businessSchema = new mongoose.Schema(schema);
+
+const Business = mongoose.model('Business', businessSchema);
+
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/test');
+
+  const results = await searchYelp({ location: '11214' });
+
+  for (const result of results) {
+    const record = new Business(result);
+    await record.save();
+  }
+}
+
+main().catch(err => console.log(err));
---
+++ schema.js
@@ -0,0 +1,103 @@
+const schema = {
+    "id": {
+      "type": "string"
+    },
+    "alias": {
+      "type": "string"
+    },
+    "name": {
+      "type": "string"
+    },
+    "image_url": {
+      "type": "string"
+    },
+    "is_closed": {
+      "type": "boolean"
+    },
+    "url": {
+      "type": "string"
+    },
+    "review_count": {
+      "type": "number"
+    },
+    "categories": {
+      "type": "array",
+      "items": {
+        "type": "object",
+        "properties": {
+          "alias": {
+            "type": "string"
+          },
+          "title": {
+            "type": "string"
+          }
+        }
+      }
+    },
+    "rating": {
+      "type": "number"
+    },
+    "coordinates": {
+      "type": "object",
+      "properties": {
+        "latitude": {
+          "type": "number"
+        },
+        "longitude": {
+          "type": "number"
+        }
+      }
+    },
+    "transactions": {
+      "type": "array",
+      "items": {
+        "type": "string"
+      }
+    },
+    "price": {
+      "type": "string"
+    },
+    "location": {
+      "type": "object",
+      "properties": {
+        "address1": {
+          "type": "string"
+        },
+        "address2": {
+          "type": "string"
+        },
+        "address3": {
+          "type": "string"
+        },
+        "city": {
+          "type": "string"
+        },
+        "zip_code": {
+          "type": "string"
+        },
+        "country": {
+          "type": "string"
+        },
+        "state": {
+          "type": "string"
+        },
+        "display_address": {
+          "type": "array",
+          "items": {
+            "type": "string"
+          }
+        }
+      }
+    },
+    "phone": {
+      "type": "string"
+    },
+    "display_phone": {
+      "type": "string"
+    },
+    "distance": {
+      "type": "number"
+    }
+  }
+
+module.exports = schema;
